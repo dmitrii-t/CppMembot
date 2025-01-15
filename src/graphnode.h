890073be20e4c08e4e2b1c0f62fc31c5a5d0d73a
Dmitrii Tcimbal <dtcimbal@gmail.com>
2025-01-16 07:34:27 +1100

    Makes ChatLogic not to own GraphEdges but transfer ownership over created edges to GraphNode

--- src/graphnode.h
+++ src/graphnode.h
@@ -3,7 +3,7 @@
 
 #include <string>
 #include <vector>
-
+#include <memory>
 #include "chatbot.h"
 
 // forward declarations
@@ -15,7 +15,7 @@ private:
     ////
 
     // data handles (owned)
-    std::vector<GraphEdge*> _childEdges;  // edges to subsequent nodes
+    std::vector<std::unique_ptr<GraphEdge>> _childEdges;  // edges to subsequent nodes
 
     // data handles (not owned)
     std::vector<GraphEdge*> _parentEdges;  // edges to preceding nodes
@@ -51,7 +51,7 @@ public:
     // proprietary functions
     void AddToken(std::string token);  // add answers to list
     void AddEdgeToParentNode(GraphEdge* edge);
-    void AddEdgeToChildNode(GraphEdge* edge);
+    void AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge);
 
     //// STUDENT CODE
     ////
